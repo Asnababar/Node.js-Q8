5b7c7dbc6c1a2d33da8eb3d1ff52f354c80c475a
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-17 21:23:39 +0500

    Node.js Q8

---
+++ Q8.js
@@ -0,0 +1,7 @@
+"use strict";
+// You should create four lines that look like this console.log(5+3). your output should simply be four lines with the number 8 operating onec on each lines
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(5 + 3);
+console.log(18 - 10);
+console.log(2 * 4);
+console.log(16 / 2);
